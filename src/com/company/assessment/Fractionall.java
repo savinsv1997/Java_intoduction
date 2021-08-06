1509906445851fa6dbd1e7252d45081db93003dc
savin <savinsv1997@gmail.com>
2021-08-06 21:38:50 +0530

    Adding Factorial of the number

--- src/com/company/assessment/Fractionall.java
+++ src/com/company/assessment/Fractionall.java
@@ -1,4 +1,31 @@
 package com.company.assessment;
 
+import java.util.Scanner;
+
 public class Fractionall {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = in.nextInt();
+        int i,Factor = 1;
+        if(num<0){
+            System.out.println("Its a negative number");
+            return;
+        }else if (num == 0){
+            System.out.println("Factorial of o is 1");
+            return;
+        }
+        else{
+            for(i=1;i <= num ; i++)
+
+                Factor = Factor * i;
+
+
+
+        }
+        System.out.println("Factorial is : "+ Factor);
+
+
+    }
+
 }
